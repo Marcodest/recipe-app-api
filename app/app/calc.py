8023ed225379dad490a685c7297236d36669e1b3
Marcodest <marcoerik@gmail.com>
2024-01-14 10:53:34 +0100

    Install Postgress

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator functions
+docker-compose run --rm app sh -c "python manage.py test"
+"""
+
+
+def add(x, y):
+    """Add x and y and return result."""
+    return x + y
+
+def subtract(x, y):
+    """Subtract x and y and return result."""
+    return x - y
