719478cdb982c17a32d4b6f9c5ff1bcb92c1f8a4
Marcodest <marcoerik@gmail.com>
2024-01-30 20:14:20 +0100

    Image API Part 2

--- app/core/models.py
+++ app/core/models.py
@@ -12,12 +12,14 @@ from app import settings
 import uuid
 import os
 
+
 def recipe_image_file_path(instance, filename):
     """Generate file path for new recipe image."""
     ext = os.path.splitext(filename)[1]
     filename = f'{uuid.uuid4()}{ext}'
 
     return os.path.join('uploads', 'recipe', filename)
+
 
 class UserManager(BaseUserManager):
     """Manager for users."""
